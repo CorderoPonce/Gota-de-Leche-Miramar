7d4b6f3c5c4f72e2deefe6d4ede13defc7b59666
CorderoPonce <127542303+CorderoPonce@users.noreply.github.com>
2026-07-05 23:09:19 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Gota-de-Leche-Miramar/',
   plugins: [vue()],
   server: {
     allowedHosts: true 
